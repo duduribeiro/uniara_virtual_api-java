7f8c711892236a71f5872ac3ba45f424e1a8210e
Carlos Eduardo Ribeiro <mail@carlosribeiro.me>
2015-11-18 01:32:10 -0300

    First method, login and return token :fireworks: (need to refactor EVERYTHING.. it is only a poc)

--- src/main/java/me/carlosribeiro/uniara_virtual_api/UniaraVirtualAPI.java
+++ src/main/java/me/carlosribeiro/uniara_virtual_api/UniaraVirtualAPI.java
@@ -1,4 +1,49 @@
 package me.carlosribeiro.uniara_virtual_api;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
 public class UniaraVirtualAPI {
+    public String login(String ra, String password) {
+        HttpClient httpClient =  HttpClientBuilder.create().build();
+        HttpPost postRequest = new HttpPost(
+                "https://uniara-virtual-api.herokuapp.com/login"
+        );
+        try {
+            String json = "{\"ra\":\"" + ra + "\", \"password\":\"" + password + "\"}";
+            StringEntity input = new StringEntity(json);
+
+            input.setContentType("application/json");
+            postRequest.setEntity(input);
+
+            HttpResponse response = httpClient.execute(postRequest);
+
+
+            if (response.getStatusLine().getStatusCode() != 201) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + response.getStatusLine().getStatusCode());
+            }
+            return EntityUtils.toString(response.getEntity());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
 }
--- src/test/java/me/carlosribeiro/uniara_virtual_api/UniaraVirtualAPITest.java
+++ src/test/java/me/carlosribeiro/uniara_virtual_api/UniaraVirtualAPITest.java
@@ -1,7 +1,14 @@
 package me.carlosribeiro.uniara_virtual_api;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class UniaraVirtualAPITest {
-
+    @Test
+    public void login() {
+        UniaraVirtualAPI client = new UniaraVirtualAPI();
+        assertEquals(client.login("ra", "secret"), "");
+        //how mock this shit?
+    }
 }
